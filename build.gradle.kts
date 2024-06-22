3d345b4a791956b6a7b2a9edb7ba79eba7021094
Yannick Lamprecht <yannicklamprecht@live.de>
2024-06-22 13:37:11 +0200

    migration to gradle kts and add gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+	`java`
+	id("org.springframework.boot")  version "3.3.1"
+	id("io.spring.dependency-management") version "1.1.5"
+	kotlin("jvm") version "2.0.0"
+}
+
+group = "io.github.opendme"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(22)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.projectreactor:reactor-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+	useJUnitPlatform()
+}

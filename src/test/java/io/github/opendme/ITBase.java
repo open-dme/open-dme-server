5f79def84b12f03b42895c0d3b2a7e0c82db5512
Yannick Lamprecht <yannicklamprecht@live.de>
2024-07-19 16:55:23 +0200

    fix security in tests by mocking in memory

--- src/test/java/io/github/opendme/ITBase.java
+++ src/test/java/io/github/opendme/ITBase.java
@@ -4,6 +4,8 @@ import io.github.opendme.server.entity.DepartmentRepository;
 import io.github.opendme.server.entity.MemberRepository;
 import io.github.opendme.server.entity.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootIntegrationTest
@@ -16,5 +18,8 @@ public class ITBase {
     protected MemberRepository memberRepository;
     @Autowired
     protected SkillRepository skillRepository;
+
+    @MockBean
+    protected InMemoryClientRegistrationRepository inMemoryClientRegistrationRepository;
 
 }
